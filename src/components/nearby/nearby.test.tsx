2abea3cb46806d2cc77e0ad428af9806d69743e2
Clayton Miller <cmiller@synergybis.com>
2022-02-16 09:59:05 -0500

    getting coverage up

--- src/components/nearby/nearby.test.tsx
+++ src/components/nearby/nearby.test.tsx
@@ -29,6 +29,41 @@ describe("Application Page: Search Page", () => {
     });
   });
 
+  it("should handle rendering the component.", async () => {
+    getAll.mockResolvedValue({
+      data: [
+        {
+          name: "Here",
+          county: "Fairfax",
+          state: "VA",
+          city: "",
+          street: "",
+          zipCode: 20121,
+          latitude: 1,
+          longitude: 1,
+          chemicals: [
+            {
+              name: "Iron",
+              healthEffects: "none",
+            },
+          ],
+        },
+      ],
+      status: 200,
+    });
+    render(
+      <BrowserRouter>
+        <NearbyPage />
+      </BrowserRouter>
+    );
+
+    const inputs = await screen.findAllByRole("textbox");
+
+    inputs.forEach((search) => {
+      expect(search).toBeInTheDocument();
+    });
+  });
+
   it("should handle user interaction: 'search' action.", async () => {
     render(
       <BrowserRouter>

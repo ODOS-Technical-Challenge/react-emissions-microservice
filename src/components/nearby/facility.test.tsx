663b8741f1c61a7eaede6094aa3195a613681261
Clayton Miller <46973749+cmille62@users.noreply.github.com>
2022-02-16 10:02:52 -0500

    increasing test coverage
    * fixed test
    
    * fixed test
    
    * added some styling, and components
    
    * added ability to see the health effects and chemicals
    
    * added the ability to filter facilities based on health effect
    
    * getting coverage up
    
    Co-authored-by: Clayton Miller <cmiller@synergybis.com>

--- src/components/nearby/facility.test.tsx
+++ src/components/nearby/facility.test.tsx
@@ -5,27 +5,39 @@ import { FacilityType } from "../../types";
 import { Facility } from "./facility.component";
 
 describe("Nearby Component: Facility", () => {
+  const data: FacilityType = {
+    name: "Here",
+    county: "Fairfax",
+    state: "VA",
+    city: "",
+    street: "",
+    zipCode: 20121,
+    latitude: 1,
+    longitude: 1,
+    chemicals: [
+      {
+        name: "Iron",
+        healthEffects: "none",
+      },
+    ],
+  };
   it("should handle rendering component.", async () => {
-    const data: FacilityType = {
-      name: "Here",
-      county: "Fairfax",
-      state: "VA",
-      city: "",
-      street: "",
-      zipCode: 20121,
-      latitude: 1,
-      longitude: 1,
-      chemicals: [
-        {
-          name: "Iron",
-          healthEffects: "none",
-        },
-      ],
-    };
-
     render(<Facility data={data} />);
 
     const name = await screen.findByText(data.name);
     expect(name).toBeInTheDocument();
+  });
+
+  it("should handle filtering component.", async () => {
+    render(<Facility data={data} query={"none"} />);
+
+    const name = await screen.findByText(data.name);
+    expect(name).toBeInTheDocument();
+  });
+
+  it("should handle filtering component.", async () => {
+    const { container } = render(<Facility data={data} query={"death"} />);
+
+    expect(container.innerHTML).toEqual("");
   });
 });

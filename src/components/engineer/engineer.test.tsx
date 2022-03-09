c47de7aa4d7e38ad5df00f59b48d1dec535d391e
Almas Barzhaxynov <almas.barzhaxynov@uscg.mil>
2022-03-09 11:19:53 -0500

    updated tests

--- src/components/engineer/engineer.test.tsx
+++ src/components/engineer/engineer.test.tsx
@@ -12,36 +12,31 @@ const getAll = ExposureApi.getAll as jest.MockedFunction<
   typeof ExposureApi.getAll
 >;
 
-const setup = () => {
-  const utils = render(
-    <BrowserRouter>
-      <EngineerPage />
-    </BrowserRouter>
-  );
-  const input = utils.getByPlaceholderText("Search by zip");
-  const options = utils.getByText("Exposure Type");
-  return {
-    input,
-    options,
-    ...utils,
-  };
-};
-
 describe("Application Page: Search Page", () => {
   beforeEach(() => {
     getAll.mockResolvedValue({ data: [], status: 200 });
   });
 
   it("should handle rendering the component.", async () => {
-    const screen = setup();
+    const screen = render(
+      <BrowserRouter>
+        <EngineerPage />
+      </BrowserRouter>
+    );
     const search = await screen.findByText("Exposure Type");
     expect(search).toBeInTheDocument();
   });
 
   it("calls setLocation", async () => {
-    const screen = setup();
-    userEvent.type(screen.input, "22031");
-    userEvent.click(screen.options);
-    expect(screen.input).toBeInTheDocument();
+    const screen = render(
+      <BrowserRouter>
+        <EngineerPage />
+      </BrowserRouter>
+    );
+    const input = screen.getByPlaceholderText("Search by zip");
+    const options = screen.getByText("Exposure Type");
+    userEvent.type(input, "22031");
+    userEvent.click(options);
+    expect(input).toBeInTheDocument();
   });
 });
